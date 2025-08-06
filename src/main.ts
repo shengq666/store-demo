40d14a6b8f82f230bcb6d24e1840ba60f580e8e9
shengq <shengq007@outlook.com>
2025-08-06 10:14:26 +0800

    feat: knova

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueKonva from 'vue-konva'
+
+const app = createApp(App)
+app.use(VueKonva)
+app.mount('#app')
